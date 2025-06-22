cf465a8f47eb2a42e153734b6a2d84c807a2b6bc
medeirosavio <savio.medeiros@ccc.ufcg.edu.br>
2025-06-22 18:01:53 -0300

    Criando modelos de entidades

---
+++ src/main/java/agroforte/model/Operacao.java
@@ -0,0 +1,39 @@
+package agroforte.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Operacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate dataInicio;
+    private LocalDate dataEmissao;
+    private LocalDate dataFim;
+
+    private Integer quantidadeParcelas;
+    private LocalDate dataPrimeiraParcela;
+    private Integer tempoCarencia; // em meses
+
+    private BigDecimal valorOperacao;
+    private BigDecimal taxaMensal;
+
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+
+    @OneToMany(mappedBy = "operacao", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Parcela> parcelas;
+}
+
